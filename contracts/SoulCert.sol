98a19dae88b418ccc2a34b3d13077a67ca1b7bee
mnm458 <mohaknainmalhotra@gmail.com>
2022-10-23 22:25:44 +1100

    soulcert

--- contracts/SoulCert.sol
+++ contracts/SoulCert.sol
@@ -4,3 +4,4 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
