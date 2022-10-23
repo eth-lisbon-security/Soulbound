f6343b9b89a16cce7cfdb69cc2a9f7377f2851a1
mnm458 <mohaknainmalhotra@gmail.com>
2022-10-23 23:19:22 +1100

    OZ contract

--- contracts/SoulCert.sol
+++ contracts/SoulCert.sol
@@ -20,6 +20,9 @@ contract SoulCert is ERC721, ERC721URIStorage, Ownable {
         _setTokenURI(tokenId, uri);
     }
 
+    function _beforeTokenTransfer(
+       
+        }
     // The following functions are overrides required by Solidity.
 
     function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
