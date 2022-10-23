17af3392171758ba976bcd931b096281da0dfe10
mnm458 <mohaknainmalhotra@gmail.com>
2022-10-23 22:26:03 +1100

    soulcert

--- contracts/SoulCert.sol
+++ contracts/SoulCert.sol
@@ -7,5 +7,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract SoulCert is ERC721, ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+
 
 }
