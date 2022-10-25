7b56ef06bbbeedacf1ae60fe4ab2f706693ea59d
mnm458 <mohaknainmalhotra@gmail.com>
2022-10-25 22:54:27 +1100

    deployment fix

--- scripts/Ballot/deployment.ts
+++ scripts/Ballot/deployment.ts
@@ -19,9 +19,9 @@ async function main() {
   const wallet =
     process.env.MNEMONIC && process.env.MNEMONIC.length > 0
       ? ethers.Wallet.fromMnemonic(process.env.MNEMONIC)
-      : new ethers.Wallet(process.env.PRIVATE_KEY ?? EXPOSED_KEY);
+      : new ethers.Wallet(process.env.PRIVATE_KEY);
   console.log(`Using address ${wallet.address}`);
-  const provider = ethers.providers.getDefaultProvider("ropsten");
+  const provider = ethers.providers.getDefaultProvider("goerli");
   const signer = wallet.connect(provider);
   const balanceBN = await signer.getBalance();
   const balance = Number(ethers.utils.formatEther(balanceBN));
